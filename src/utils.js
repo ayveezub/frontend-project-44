0b65f41c4ca750466c452801c82b49d993f8eedc
ayveezub <ayveezub@gmail.com>
2024-03-28 14:25:28 +0300

    refactor: rewrite getRandomInt()

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,11 @@
-const getRandomInt = (min = 0, max = 100) => Math.floor(Math.random() * (max - min + 1) + min);
+const randomInt = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
+
+const getRandomInt = (...range) => {
+  if (range.length === 0) return randomInt(0, 100);
+  if (range.length === 1) return randomInt(0, range[0]);
+  if (range.length === 2) return randomInt(range[0], range[1]);
+  throw new Error(`Invalid range: ${range}`);
+};
 
 const isEven = (number) => number % 2 === 0;
 
